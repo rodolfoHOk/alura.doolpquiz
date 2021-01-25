ca55839fa5ba9357f154782e270e9e3b43861363
rodolfoHOk <rodolfo.okino@gmail.com>
2021-01-25 14:11:15 -0300

    final a aula 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
 
+/* padrao original
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
@@ -7,19 +9,50 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 `
+*/
 
+// obs: instalamos o vscode-styled-components para melhor visualizacao
+// copiamos o reset base para o global style
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  /* New styles */
+  display: flex;
+  flex-direction: column;
+  font-family: 'Lato', sans-serif;
+  // Deixa branco no começo
+  color: ${({ theme }) => theme.colors.contrastText};
+}
+html, body {
+  min-height: 100vh;
+}
+#__next {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+`
+
+/* padrao original
 const theme = {
   colors: {
     primary: '#0070f3',
   },
 }
+*/
+
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <GlobalStyle />
+          <Component {...pageProps} />
       </ThemeProvider>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,65 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/Components/Widget'
+import Footer from '../src/Components/Footer'
+import GitHubCorner from '../src/Components/GitHubCorner'
+import QuizBackground from '../src/Components/QuizBackground'
+import QuizLogo from '../src/Components/QuizLogo'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+/* importado do arquivo agora
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+*/
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    //<BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
+        <QuizLogo/>
+
+        <Widget>
+          <Widget.Header>
+            <h1>
+              olah
+            </h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>dno cneoivq vrnoewn...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>
+              Quizes da Galera
+            </h1>
+
+            <p>dno cneoivq vrnoewn...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto"/>
+    </QuizBackground>
+    //</BackgroundImage>
+  );
 }
